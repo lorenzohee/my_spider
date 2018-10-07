a02dd5831abd6b9dbd27b8b3904f793fd96826cd
lorenzohee <yongqinghee@163.com>
2018-10-07 15:39:35 +0800

    fit the bug

--- sql.sql
+++ sql.sql
@@ -6,4 +6,7 @@ article_id integer,
 url_from text,
 is_published integer,
 articleType_id integer,
-create_time datetime)
+create_time datetime,
+update_time datetime,
+source_id integer,
+num_of_view integer)
